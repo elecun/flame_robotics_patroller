296824726bb4164a3d5d97b5cabace12c67701ee
elecun <elecun2@gmail.com>
2025-04-17 22:09:15 +0900

    fixing

--- components/synerex.rtk.receiver/synerex.rtk.receiver.cc
+++ components/synerex.rtk.receiver/synerex.rtk.receiver.cc
@@ -20,6 +20,12 @@ bool synerex_rtk_receiver::on_init(){
         /* get parameters from profile */
         json parameters = get_profile()->parameters();
 
+        string port = parameters.value("port", "/dev/ttyS0");
+        unsigned int baudrate = parameters.value("baudrate", 115200);
+        char err = _serial.openDevice(port.c_str(), baudrate);
+        if(err!=1)
+            return false;
+
         
 
     }
@@ -37,6 +43,8 @@ void synerex_rtk_receiver::on_loop(){
 
 
 void synerex_rtk_receiver::on_close(){
+
+    _serial.closeDevice();
 
 }
 
--- components/synerex.rtk.receiver/synerex.rtk.receiver.hpp
+++ components/synerex.rtk.receiver/synerex.rtk.receiver.hpp
@@ -9,37 +9,41 @@
  * 
  */
 
- #ifndef FLAME_SYNEREX_RTK_RECEIVER_HPP_INCLUDED
- #define FLAME_SYNEREX_RTK_RECEIVER_HPP_INCLUDED
- 
- #include <flame/component/object.hpp>
- #include <map>
- #include <unordered_map>
- #include <vector>
- #include <thread>
- #include <string>
- #include <atomic>
-  
- using namespace std;
- 
- class synerex_rtk_receiver : public flame::component::object {
-     public:
-         basler_gige_cam_grabber() = default;
-         virtual ~basler_gige_cam_grabber() = default;
- 
-         /* default interface functions */
-         bool on_init() override;
-         void on_loop() override;
-         void on_close() override;
-         void on_message() override;
- 
- 
-     private:
-         atomic<bool> _worker_stop {false};
- 
- }; /* class */
- 
- EXPORT_COMPONENT_API
- 
- 
- #endif
+#ifndef FLAME_SYNEREX_RTK_RECEIVER_HPP_INCLUDED
+#define FLAME_SYNEREX_RTK_RECEIVER_HPP_INCLUDED
+
+#include <flame/component/object.hpp>
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <thread>
+#include <string>
+#include <atomic>
+#include "serial/serialib.h"
+
+using namespace std;
+
+class synerex_rtk_receiver : public flame::component::object {
+    public:
+    synerex_rtk_receiver() = default;
+        virtual ~synerex_rtk_receiver() = default;
+
+        /* default interface functions */
+        bool on_init() override;
+        void on_loop() override;
+        void on_close() override;
+        void on_message() override;
+
+    private:
+        /* serial */
+        serialib _serial;
+
+        atomic<bool> _worker_stop {false};
+
+
+}; /* class */
+
+EXPORT_COMPONENT_API
+
+
+#endif
