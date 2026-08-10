7199f51f34653112d677489edfe7940dc4c7f2e7
elecun <elecun2@gmail.com>
2026-08-10 14:47:38 +0900

    removed unused code files

--- APROS/core/plugin/__init__.py
+++ APROS/core/plugin/__init__.py
@@ -2,25 +2,11 @@
 plugin package initialization.
 """
 from core.plugin.base import BasePlugin
-from core.plugin.global_path_planner import BaseGlobalPathPlanner
-from core.plugin.astar_planner import AStarPlanner
-from core.plugin.base_planner import GlobalPlannerBase, StaticGlobalPlanner, DynamicGlobalPlanner
-from core.plugin.local_planner import BaseLocalPlanner, DWAPlanner
-from core.plugin.global_planner import BaseGlobalPlanner
-from core.plugin.cost_map import CostMap
-from core.plugin.job_planner import JobPlanner, MissionState
+from core.plugin.drive_executor import DriveExecutor
+from core.plugin.mast_executor import MastExecutor
 
 __all__ = [
     "BasePlugin",
-    "BaseGlobalPathPlanner",
-    "AStarPlanner",
-    "GlobalPlannerBase",
-    "StaticGlobalPlanner",
-    "DynamicGlobalPlanner",
-    "BaseLocalPlanner",
-    "DWAPlanner",
-    "BaseGlobalPlanner",
-    "CostMap",
-    "JobPlanner",
-    "MissionState",
+    "DriveExecutor",
+    "MastExecutor",
 ]
